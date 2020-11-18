f7d272a9c50d78d9e95e786c1707577d98614ef8
Mark <m.siegrist92@gmail.com>
2020-11-18 16:42:31 -0500

    basic file upload and save to directory functionality

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const pipeline = promisify(require('stream').pipeline);
 const User = require('../db/schemas/user.js');
 
 const router = new express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploads)
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
-
-const upload = multer({storage: storage});
 
 router.get('/user', async (req, res) => {
   await res.send('hey fucker')
@@ -25,7 +16,6 @@ router.get('/user', async (req, res) => {
 router.post('/user/register', async (req, res) => {
 
   let data = req.body;
-  console.log(data);
   const check_email = await User.findOne({email: data.email});
   const check_user = await User.findOne({username: data.username})
   if(check_email != null ){
@@ -37,23 +27,36 @@ router.post('/user/register', async (req, res) => {
   }
 
   const user = new User({
-    //turn req.body into User here
-    //need to wire up multer to grab avatar image first to try
-    email: data.email,
-    password: data.password,
-    username: data.username,
+      email: data.email,
+      password: data.password,
+      username: data.username,
+      signature: 'this is at emp signature'
   })
-
-  router.post('/user/avatar', upload.single('image'), (req, res) => {
-    //find user by id
-    console.log(req.file);
-    //update user avatar to the image uploaded
-  })
-
+  console.log(user)
   try {
     await user.save();
     res.status(201).send();
   } catch (error) {
+    res.status(500).send();
+  }
+
+})
+
+  const upload = multer();
+  router.post('/user/:id/avatar', upload.single('file'), async (req, res, next) => {
+    //find user by id
+    console.log(req.file);
+    //update user avatar to the image uploaded
+    await pipeline(req.file.stream, fs.createWriteStream(`${__dirname}/../../uploads/avatarimage.jpg`));
+
+    // const user = await User.findById(req.params.id);
+    // user.avatar = req.body;
+  try {
+    // await user.save();
+    // console.log(user);
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
     res.status(500).send(error);
   }
 })
