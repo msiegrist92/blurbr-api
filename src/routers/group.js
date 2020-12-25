a03254f9e93433aec71f53e9b8abf7c4c13fd534
Mark <m.siegrist92@gmail.com>
2020-12-25 17:50:26 -0500

    create two new endpoints in /group for creating and approving group join requests - format mailgun email template with links to view and approve user

--- src/routers/group.js
+++ src/routers/group.js
@@ -72,32 +72,75 @@ router.post('/group', async (req, res) => {
   }
 })
 
+//this endpoint receives user and group id from token and sends email to group owner
 router.post('/group/joinrequest/:token', async (req, res) => {
-  console.log(req.params.token)
 
-  const token = Token.findOne({token: req.params.token});
-  if(!token){
-    return res.status(403).send("You must log in to request a group");
+
+  const token_decode = jwt.verify(req.params.token, process.env.JWT_SECRET);
+
+  const {user_id, group_id} = token_decode;
+
+  const user = await User.findById(user_id);
+  const group = await Group.findById(group_id);
+
+  const owner = await User.findById(group.owner);
+
+  console.log(user, group, owner)
+
+  if(user === null || group === null || owner === null){
+    return res.status(403).send()
   }
+  const approve_token = jwt.sign({user_id, group_id}, process.env.JWT_GROUP_KEY);
 
-  const user_id = jwt.verify(req.params.token, process.env.JWT_SECRET)._id;
+  const user_url = 'http://' + process.env.CLIENT_URL + '/users/' + user._id;
+  console.log(user_url)
+  const approve_url = 'http://' + process.env.API_URL + '/group/approvereq/' + approve_token;
+  console.log(user_url)
 
   const domain = 'sandbox5da1889582d94201a8cdecedb7a36b1d.mailgun.org';
   const mg = mailgun({apiKey: process.env.MAILGUN_KEY, domain});
   const data = {
     from : "Blurbr Groups <groups@blurbr.com>",
     to: 'm.siegrist92@gmail.com',
-    subject: "Good ol' Mailgun Test",
-    text: "Testing testing 1 2"
+    subject: `Blurbr User ${user.username} would like to join your group ${group.name}`,
+    html: `<html><body><p>Hello there ${owner.username} - ${user.username} would like to join one of your groups.</p>
+      <button><a href="${user_url}">View Profile</a></button>
+      <button><a href="${approve_url}">Approve Request</a></button>
+      </body>
+      </html> `
   };
   mg.messages().send(data, (err, body) => {
     console.log(body)
   })
   return res.status(200).send('Email sent')
 
-
 })
 
+router.get('/group/approvereq/:token', async (req, res) => {
+
+  const token_decode = jwt.verify(req.params.token, process.env.JWT_GROUP_KEY);
+
+  const {user_id, group_id} = token_decode;
+
+  const user = await User.findById(user_id);
+  const group = await Group.findById(group_id);
+
+  if(user === null || group === null){
+    return res.status(403).send('Unauthorized back up')
+  }
+
+  const members = group.users;
+
+  if(members.includes(user._id)){
+    return res.send('User is already a member of your group');
+  }
+
+  group.users.push(user._id);
+  await group.save();
+
+  return res.send('User approved!')
+
+})
 
 
 module.exports = router;
