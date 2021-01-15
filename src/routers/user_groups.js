db7db45be498b3bd2706822013768e33149272d0
Mark <m.siegrist92@gmail.com>
2021-01-14 20:36:10 -0500

    rename user_groups to /member_groups which was its functionality - rewrite /user_groups to return only groups the user owns

--- src/routers/user_groups.js
+++ src/routers/user_groups.js
@@ -9,7 +9,32 @@ const mongooseQueries = require('../lib/mongooseQueries');
 
 const router = new express.Router();
 
+//returns groups which user owns
+
 router.get('/user_groups/:id', async (req, res) => {
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    const groups = await Group.find({owner : req.params.id}).lean();
+
+    //var to loop over, Schema to search, ref field name, attach point of var
+    await mongooseQueries.loopFindRefAndAttach(groups, User, 'owner', 'owner');
+
+    await mongooseQueries.loopFindRefLastIndexAndAttach(groups, Topic, 'topics', 'most_recent');
+
+    console.log(groups);
+
+    return res.status(200).send(groups);
+
+  } catch (err) {
+    return res.status(500).send(err)
+  }
+})
+
+//returns groups which user is a member of
+
+router.get('/member_groups/:id', async (req, res) => {
 
   try {
     const user = await User.findById(req.params.id);
@@ -29,5 +54,7 @@ router.get('/user_groups/:id', async (req, res) => {
     return res.status(500).send(err)
   }
 })
+
+
 
 module.exports = router;
